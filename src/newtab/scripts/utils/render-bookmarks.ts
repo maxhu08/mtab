64a1790bdc8eb7f70ccabfd73dd6d7d554d02d15
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-03-12 16:59:34 -0500

    fix

--- src/newtab/scripts/utils/render-bookmarks.ts
+++ src/newtab/scripts/utils/render-bookmarks.ts
@@ -9,7 +9,7 @@ export const renderBookmarks = (config: Config) => {
     <div id="bookmark-${
       bookmark.name
     }-${index}" class="hover:bg-white/20 cursor-pointer bg-neutral-800 rounded-md h-20 md:h-32 overflow-hidden ${
-      config.animations ? `${config.animations.animationClass} opacity-0` : ""
+      config.animations.enabled === "on" ? `${config.animations.animationClass} opacity-0` : ""
     }" ${config.animations ? `style="animation-delay: ${(index + 2) * 50 + 50}ms;"` : ""}>
       <div class="${bookmark.colorClass} h-1"></div>
       <div class="p-1 md:p-2 grid place-items-center h-full">
