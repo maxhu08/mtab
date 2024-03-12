86155be4bcf44adcaa47896b76d6b18539930b7d
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-03-12 18:42:59 -0500

    fix

--- src/newtab/scripts/utils/animations.ts
+++ src/newtab/scripts/utils/animations.ts
@@ -4,6 +4,19 @@ import { messageEl, searchContainerEl } from "../ui";
 export const addAnimations = (animations: Config["animations"]) => {
   if (!animations.enabled) return;
 
+  // Define your CSS rules as plaintext
+  const animationCss = `
+* {
+  transition-duration: 250ms;
+}`;
+
+  const styleElement = document.createElement("style");
+  styleElement.type = "text/css";
+  styleElement.appendChild(document.createTextNode(animationCss));
+  document.head.appendChild(styleElement);
+
+  console.log(styleElement);
+
   messageEl.style.animationDelay = "0ms";
   messageEl.classList.add(animations.animationClass);
 
