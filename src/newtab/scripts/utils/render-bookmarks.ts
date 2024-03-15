7fbf874b72ad04d37105258e4e06969855cc7911
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-03-15 17:52:40 -0500

    improve ui

--- src/newtab/scripts/utils/render-bookmarks.ts
+++ src/newtab/scripts/utils/render-bookmarks.ts
@@ -6,8 +6,10 @@ export const renderBookmarks = (config: Config) => {
   config.bookmarks.forEach((bookmark, index) => {
     bookmarksContainerEl.innerHTML += `
   <a href="${bookmark.url}" rel="noopener noreferrer">
-    <div id="bookmark-${bookmark.name}-${index}" class="hover:bg-white/20 cursor-pointer ${
-      config.uiStyle === "glass" ? "glass-effect" : "bg-neutral-800"
+    <div id="bookmark-${bookmark.name}-${index}" class="${
+      config.uiStyle === "glass" ? "hover:bg-white/20" : "hover:bg-neutral-800"
+    } cursor-pointer ${
+      config.uiStyle === "glass" ? "glass-effect" : "bg-neutral-900"
     } rounded-md h-20 md:h-32 overflow-hidden ${
       config.animations.enabled ? `${config.animations.animationClass} opacity-0` : ""
     }" ${config.animations ? `style="animation-delay: ${(index + 2) * 50 + 50}ms;"` : ""}>
