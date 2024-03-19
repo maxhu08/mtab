45f80d36fef4f9afc2444045bb450e3b7bbd97bc
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-03-19 18:15:08 -0500

    fix for time message

--- src/newtab/scripts/utils/set-message.ts
+++ src/newtab/scripts/utils/set-message.ts
@@ -15,7 +15,7 @@ export const setMessage = (type: MessageType, customText: string, username: stri
       break;
     }
     case "time-12": {
-      setTimeMessage("24hr");
+      setTimeMessage("12hr");
       break;
     }
     case "time-24": {
@@ -43,16 +43,16 @@ export const setTimeMessage = (format: "12hr" | "24hr" = "12hr") => {
 
   const day = days[currentDate.getDay()];
   let hours = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
+  const minutes = currentDate.getMinutes()
 
   let timeString;
   if (format === "12hr") {
     const meridiem = hours >= 12 ? "PM" : "AM";
     hours = hours % 12 || 12;
 
-    timeString = `${day} ${hours}:${minutes} ${meridiem}`;
+    timeString = `${day} ${hours}:${minutes < 10 && "0" + minutes} ${meridiem}`;
   } else if (format === "24hr") {
-    timeString = `${day} ${hours}:${minutes}`;
+    timeString = `${day} ${hours}:${minutes < 10 && "0" + minutes}`;
   } else {
     timeString = "error: use 12hr or 24hr format";
   }
