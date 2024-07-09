c19223fddc053e6d3d690f112d0b6194a4cb72ab
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-08 21:18:03 -0500

    popup stuff

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,9 +1,25 @@
+const logo = document.getElementById("mtab-logo") as HTMLImageElement;
+
+logo.classList.add("animate-up-bouncy");
+
+logo.addEventListener(
+  "animationend",
+  () => {
+    logo.classList.replace("animate-up-bouncy", "animate-float");
+  },
+  {
+    once: true
+  }
+);
+
 document.getElementById("options-button")!.addEventListener("click", () => {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
     return;
   } else {
+    // @ts-expect-error
     if (browser.runtime.openOptionsPage) {
+      // @ts-expect-error
       browser.runtime.openOptionsPage();
       return;
     }
