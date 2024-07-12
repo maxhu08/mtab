ffd44be53d7e136ec9d7dd769b9ee860f226c393
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-12 15:51:45 -0500

    improve scripts

---
+++ scripts/linux/package_firefox.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+EXTENSION_VERSION=$(cat EXTENSION_VERSION.txt)
+
+mkdir -p package &&
+cd dist &&
+zip -r -FS ../package/mtab-v$EXTENSION_VERSION-firefox.zip * --exclude '*.git*'
--- scripts/package_chrome.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-EXTENSION_VERSION=$(cat EXTENSION_VERSION.txt)
-
-cd dist &&
-zip -r -FS ../mtab-v$EXTENSION_VERSION-chrome.zip * --exclude '*.git*'
