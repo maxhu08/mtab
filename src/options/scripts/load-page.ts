7977e3c0bd143fa7dbfa7a95bc8442801e8371b3
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-14 16:00:23 -0500

    organize

---
+++ src/options/scripts/load-page.ts
@@ -0,0 +1,38 @@
+import { fillInputs } from "src/options/scripts/utils/fill-inputs";
+import { listenToKeys } from "src/options/scripts/keybinds";
+import { listenToInputs } from "src/options/scripts/inputs";
+import { handleWallpaperFileUpload } from "src/options/scripts/utils/file-upload";
+import { getConfig } from "src/newtab/scripts/config";
+import { getUserAgent } from "src/util-scripts/user-agent";
+import {
+  handleMessageTypeSwitch,
+  handleWallpaperTypeSwitch,
+  handleBookmarksTypeSwitch
+} from "src/options/scripts/utils/handle-switch";
+import {
+  fixAllToggleCheckboxSections,
+  listenAllToggleCheckboxSections
+} from "src/options/scripts/utils/toggle-checkbox";
+
+export const loadPage = () => {
+  getConfig(({ config }) => {
+    fillInputs(config);
+
+    fixAllToggleCheckboxSections();
+  });
+
+  (document.getElementById("version-number-text") as HTMLSpanElement).textContent +=
+    chrome.runtime.getManifest().version;
+
+  (document.getElementById("user-agent-text") as HTMLSpanElement).textContent += getUserAgent();
+
+  listenToInputs();
+  listenToKeys();
+  handleWallpaperFileUpload();
+
+  handleMessageTypeSwitch();
+  handleWallpaperTypeSwitch();
+  handleBookmarksTypeSwitch();
+
+  listenAllToggleCheckboxSections();
+};
