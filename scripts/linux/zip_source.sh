2f82b4e21b77f4d73b6fdc887a5627126968e742
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-16 10:48:20 -0500

    fix script

--- scripts/linux/zip_source.sh
+++ scripts/linux/zip_source.sh
@@ -1,1 +1,3 @@
+EXTENSION_VERSION=$(cat EXTENSION_VERSION.txt)
+
 zip -r -FS ../mtab-v$EXTENSION_VERSION-source.zip * -x "*.git*" -x "node_modules/*" -x "assets/*" -x ".parcel-cache/*" -x "dist" -x "package/*" -x ".husky/*"
