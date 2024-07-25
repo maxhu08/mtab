136c6da8fa837e5b30885f32b0bff21f4a1d19ff
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-25 10:04:21 -0500

    fix some fileUpload wallpaper's not working (#16)

--- src/options/scripts/utils/file-upload.ts
+++ src/options/scripts/utils/file-upload.ts
@@ -6,12 +6,45 @@ export const handleWallpaperFileUpload = () => {
 
     if (file) {
       const reader = new FileReader();
-      reader.onload = function (e: any) {
-        console.log(file);
-
-        chrome.storage.local.set({ userUploadedWallpaper: e.target.result });
+      reader.onload = (e: any) => {
+        // resize image to 1920x1080 because chrome can't store really large images
+        resizeImage(e.target.result, 1920, 1080, (resizedDataUrl) => {
+          chrome.storage.local.set({ userUploadedWallpaper: resizedDataUrl });
+        });
       };
       reader.readAsDataURL(file);
     }
   });
+};
+
+const resizeImage = (
+  dataUrl: string,
+  maxWidth: number,
+  maxHeight: number,
+  callback: (resizedDataUrl: string) => void
+) => {
+  const img = new Image();
+  img.src = dataUrl;
+  img.onload = () => {
+    const canvas = document.createElement("canvas");
+    let width = img.width;
+    let height = img.height;
+
+    if (width > height) {
+      if (width > maxWidth) {
+        height = Math.round((height *= maxWidth / width));
+        width = maxWidth;
+      }
+    } else {
+      if (height > maxHeight) {
+        width = Math.round((width *= maxHeight / height));
+        height = maxHeight;
+      }
+    }
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(img, 0, 0, width, height);
+    callback(canvas.toDataURL("image/jpeg"));
+  };
 };
