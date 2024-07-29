e14dee4a01bfef74b7fb0bea4219d5808d7a4f82
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-29 12:16:15 -0500

    change logo animation

--- src/options/scripts/load-page.ts
+++ src/options/scripts/load-page.ts
@@ -15,6 +15,19 @@ import {
 } from "src/options/scripts/utils/toggle-checkbox";
 
 export const loadPage = () => {
+  const logo = document.getElementById("mtab-logo") as HTMLImageElement;
+  logo.classList.add("animate-up-bouncy");
+
+  logo.addEventListener(
+    "animationend",
+    () => {
+      logo.classList.replace("animate-up-bouncy", "animate-float");
+    },
+    {
+      once: true
+    }
+  );
+
   getConfig(({ config }) => {
     fillInputs(config);
 
