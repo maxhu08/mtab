302cc055cce577d20a08a4e9df63dafb088c542b
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-29 18:16:26 -0500

    make search bar more smooth on initial load

--- src/newtab/scripts/utils/animations.ts
+++ src/newtab/scripts/utils/animations.ts
@@ -19,4 +19,26 @@ export const addAnimations = (animations: Config["animations"]) => {
 
   searchContainerEl.style.animationDelay = "50ms";
   searchContainerEl.classList.add(animations.type);
+
+  if (animations.enabled) {
+    const computedStyle = window.getComputedStyle(searchContainerEl);
+    const animationDuration = parseFloat(computedStyle.animationDuration) * 1000;
+    searchContainerEl.addEventListener(
+      "animationstart",
+      () => {
+        // Fix weird flickering issue on firefox
+        setTimeout(() => {
+          searchContainerEl.classList.remove("opacity-0");
+        }, animationDuration - 500);
+      },
+      {
+        once: true
+      }
+    );
+
+    // Fix bookmarks disappearing if user leaves tab too quickly
+    document.addEventListener("visibilitychange", () => {
+      searchContainerEl.classList.remove("opacity-0");
+    });
+  }
 };
