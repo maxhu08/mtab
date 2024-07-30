73d88792cf72921ae8d2175af5fe086f63958042
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-07-29 21:37:25 -0500

    show icon if no wallpaper or favicon uploaded

--- src/options/scripts/utils/preview.ts
+++ src/options/scripts/utils/preview.ts
@@ -2,12 +2,20 @@ export const previewWallpaper = (wallpaper: string) => {
   // prettier-ignore
   const liveWallpaperPreviewEl = document.getElementById("live-wallpaper-preview") as HTMLDivElement;
 
-  liveWallpaperPreviewEl.innerHTML = `<img src="${wallpaper}" class="w-full h-full" />`;
+  if (!wallpaper) {
+    liveWallpaperPreviewEl.innerHTML = `<i class="text-neutral-500 text-4xl ri-prohibited-2-line"></i>`;
+  } else {
+    liveWallpaperPreviewEl.innerHTML = `<img src="${wallpaper}" class="w-full h-full" />`;
+  }
 };
 
 export const previewFavicon = (favicon: string) => {
   // prettier-ignore
   const liveFaviconPreviewEl = document.getElementById("live-favicon-preview") as HTMLDivElement;
 
-  liveFaviconPreviewEl.innerHTML = `<img src="${favicon}" class="w-full h-full" />`;
+  if (!favicon) {
+    liveFaviconPreviewEl.innerHTML = `<i class="text-neutral-500 text-4xl ri-prohibited-2-line"></i>`;
+  } else {
+    liveFaviconPreviewEl.innerHTML = `<img src="${favicon}" class="w-full h-full" />`;
+  }
 };
