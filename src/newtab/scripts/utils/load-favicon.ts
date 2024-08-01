a61c988088846eb587e36ab0dc74f7cffa791b95
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-08-01 12:50:18 -0500

    change favicon loading (#23)

--- src/newtab/scripts/utils/load-favicon.ts
+++ src/newtab/scripts/utils/load-favicon.ts
@@ -1,10 +1,12 @@
 import { FaviconType } from "src/newtab/scripts/config";
 
 export const loadFavicon = (faviconType: FaviconType) => {
-  if (faviconType !== "custom") return;
+  if (faviconType !== "custom") {
+    document.head.innerHTML += `<link id="favicon" rel="icon" href="favicon.ico" type="image/x-icon">`;
+    return;
+  }
 
   chrome.storage.local.get(["userUploadedFavicon"], (data) => {
-    const favicon = document.getElementById("favicon") as HTMLLinkElement;
-    favicon.href = data.userUploadedFavicon as string;
+    document.head.innerHTML += `<link id="favicon" rel="icon" href="${data.userUploadedFavicon}" type="image/x-icon">`;
   });
 };
