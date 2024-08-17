9db17c900ece63ed7f096ed68606c637d1ec76f2
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-08-17 10:06:06 -0500

    try to fix ui disappearing again (#27)

--- src/newtab/scripts/utils/fix-disappearing-ui.ts
+++ src/newtab/scripts/utils/fix-disappearing-ui.ts
@@ -1,5 +1,11 @@
+import { AnimationBookmarkType, AnimationSearchType } from "src/newtab/scripts/config";
 import { contentEl } from "src/newtab/scripts/ui";
 
 export const fixDisappearingUI = () => {
-  contentEl.style.opacity = "100%";
+  // for user clicks on bookmark ui disappears
+  window.addEventListener("pageshow", (e) => {
+    if (e.persisted) {
+      window.location.reload();
+    }
+  });
 };
