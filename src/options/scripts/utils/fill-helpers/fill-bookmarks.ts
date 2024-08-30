030e67d7f67360231c142250e710a6e9a12af0f0
maxhu08 <contact@maxhu.dev>
2024-08-29 20:22:57 -0500

    make user defined bookmarks automatically collapsed in options page

--- src/options/scripts/utils/fill-helpers/fill-bookmarks.ts
+++ src/options/scripts/utils/fill-helpers/fill-bookmarks.ts
@@ -38,6 +38,8 @@ export const fillBookmarksInputs = (config: Config) => {
   config.bookmarks.userDefined.forEach((_, index) => {
     handleBookmarkSettings(index);
   });
+
+  toggleCollapseAllBookmarksButtonEl.click();
 };
 
 // prettier-ignore
