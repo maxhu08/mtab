98f275df79bfe8c84332583dd0518d88452b5743
maxhu08 <contact@maxhu.dev>
2024-09-02 12:22:25 -0500

    change how bookmark favicons are fetched (#38)

--- src/newtab/scripts/utils/favicon-url.ts
+++ src/newtab/scripts/utils/favicon-url.ts
@@ -3,6 +3,10 @@ export const getFaviconURL = (url: string, userAgent: string) => {
     // prettier-ignore
     return `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(url)}&size=${64}`;
   } else {
-    return `https://www.google.com/s2/favicons?domain=${encodeURIComponent(url)}&sz=256`;
+    // google version
+    // return `https://www.google.com/s2/favicons?domain=${encodeURIComponent(url)}&sz=256`;
+
+    // duckduckgo version
+    return `https://icons.duckduckgo.com/ip3/${new URL(url).hostname}.ico`;
   }
 };
