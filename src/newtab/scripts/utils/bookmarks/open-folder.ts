a3a082cad5b975dffbb674037d9f4bc8de060244
maxhu08 <contact@maxhu.dev>
2024-12-03 21:38:52 -0600

    fix issue with opening folders and number keys

--- src/newtab/scripts/utils/bookmarks/open-folder.ts
+++ src/newtab/scripts/utils/bookmarks/open-folder.ts
@@ -1,4 +1,6 @@
 export const openFolder = (currFolderAreaEl: HTMLDivElement, openfolderAreaEl: HTMLDivElement) => {
   currFolderAreaEl.classList.replace("grid", "hidden");
+  currFolderAreaEl.setAttribute("folder-state", "closed");
   openfolderAreaEl.classList.replace("hidden", "grid");
+  openfolderAreaEl.setAttribute("folder-state", "open");
 };
