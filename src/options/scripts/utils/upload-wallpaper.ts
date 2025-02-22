844c00838f7743d11ad507d075679748da61d7dd
maxhu08 <contact@maxhu.dev>
2025-02-22 01:35:09 -0600

    remove unused

--- src/options/scripts/utils/upload-wallpaper.ts
+++ src/options/scripts/utils/upload-wallpaper.ts
@@ -1,8 +1,4 @@
-import {
-  wallpaperFileUploadInputEl,
-  wallpaperResizeHInputEl,
-  wallpaperResizeWInputEl
-} from "src/options/scripts/ui";
+import { wallpaperFileUploadInputEl } from "src/options/scripts/ui";
 import { previewWallpaper } from "src/options/scripts/utils/preview";
 import { set as idbSet, get as idbGet } from "idb-keyval";
 
@@ -30,36 +26,4 @@ export const handleWallpaperFileUpload = () => {
 export const handWallpaperFileReset = () => {
   chrome.storage.local.set({ userUploadedWallpaper: null });
   previewWallpaper(undefined);
-};
-
-const resizeImage = (
-  dataUrl: string,
-  maxWidth: number,
-  maxHeight: number,
-  callback: (resizedDataUrl: string) => void
-) => {
-  const img = new Image();
-  img.src = dataUrl;
-  img.onload = () => {
-    const canvas = document.createElement("canvas");
-    let width = img.width;
-    let height = img.height;
-
-    if (width > height) {
-      if (width > maxWidth) {
-        height = Math.round((height *= maxWidth / width));
-        width = maxWidth;
-      }
-    } else {
-      if (height > maxHeight) {
-        width = Math.round((width *= maxHeight / height));
-        height = maxHeight;
-      }
-    }
-    canvas.width = width;
-    canvas.height = height;
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    ctx.drawImage(img, 0, 0, width, height);
-    callback(canvas.toDataURL("image/jpeg"));
-  };
 };
