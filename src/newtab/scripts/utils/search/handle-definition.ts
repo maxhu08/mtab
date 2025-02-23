cee58073c1add709b6e531d8276dbc7447f762db
maxhu08 <contact@maxhu.dev>
2025-02-22 20:38:07 -0600

    improve search assist definition

--- src/newtab/scripts/utils/search/handle-definition.ts
+++ src/newtab/scripts/utils/search/handle-definition.ts
@@ -1,14 +1,15 @@
 export const handleDefinition = async (val: string) => {
-  if (val.endsWith(" def") || val.endsWith(" definition")) {
-    const word = val.replace(/ def$/, "").replace(/ definition$/, "");
+  const match = val.match(/\b(.+?)\s+def(?:i(?:n(?:i(?:t(?:i(?:o(?:n)?)?)?)?)?)?)?$/);
+  if (!match) return;
 
-    try {
-      // prettier-ignore
-      const response = await fetch(`https://raw.githubusercontent.com/open-dictionary/english-dictionary/master/${word[0]}/${word[1]}/${word}/en.json`);
-      // const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+  const word = match[1];
 
-      const data = await response.json();
-      return { type: "definition", result: data } as const;
-    } catch {}
-  }
+  try {
+    // prettier-ignore
+    const response = await fetch(`https://raw.githubusercontent.com/open-dictionary/english-dictionary/master/${word[0]}/${word[1]}/${word}/en.json`);
+    // const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+
+    const data = await response.json();
+    return { type: "definition", result: data } as const;
+  } catch {}
 };
