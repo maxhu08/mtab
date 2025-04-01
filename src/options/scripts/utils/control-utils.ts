e346f6f55e1db0aa87b53c45d74d44907906c583
maxhu08 <contact@maxhu.dev>
2025-04-01 17:30:12 -0500

    fix issues with controls position and size when resizing window and more

--- src/options/scripts/utils/control-utils.ts
+++ src/options/scripts/utils/control-utils.ts
@@ -12,27 +12,44 @@ export const showControls = () => {
   controlsContainerEl.classList.remove("hidden");
 };
 
+const updateFloatingControlsPosition = () => {
+  console.log("Updating floating controls position");
+
+  bottomOfPageSensorEl.style.height = controlsContainerEl.style.height;
+  controlsContainerEl.style.bottom = "10px";
+
+  const { left, width } = mainEl.getBoundingClientRect();
+  controlsContainerEl.style.left = `${left}px`;
+  controlsContainerEl.style.width = `${width}px`;
+};
+
 export const handleControls = () => {
-  bottomOfPageSensorEl.style.height = "120px";
+  let isIntersecting = false;
+  bottomOfPageSensorEl.style.height = controlsContainerEl.style.height;
 
   const observer = new IntersectionObserver(
     ([entry]) => {
-      if (entry.isIntersecting) {
+      isIntersecting = entry.isIntersecting;
+      if (isIntersecting) {
+        controlsContainerEl.style.removeProperty("width");
         controlsContainerEl.style.bottom = "0";
         controlsContainerEl.style.left = "0";
         controlsContainerEl.classList.replace("fixed", "relative");
-        controlsContainerEl.classList.remove("border-emerald-500", "border-2");
+        controlsContainerEl.classList.remove("drop-shadow");
       } else {
-        const { left, width } = mainEl.getBoundingClientRect();
-        controlsContainerEl.style.bottom = "10px";
-        controlsContainerEl.style.left = `${left}px`;
-        controlsContainerEl.style.width = `${width}px`;
+        updateFloatingControlsPosition();
         controlsContainerEl.classList.replace("relative", "fixed");
-        controlsContainerEl.classList.add("border-emerald-500", "border-2");
+        controlsContainerEl.classList.add("drop-shadow");
       }
     },
     { rootMargin: "0px", threshold: 0.1 }
   );
 
   if (bottomOfPageSensorEl) observer.observe(bottomOfPageSensorEl);
+
+  window.addEventListener("resize", () => {
+    if (!isIntersecting) {
+      updateFloatingControlsPosition();
+    }
+  });
 };
