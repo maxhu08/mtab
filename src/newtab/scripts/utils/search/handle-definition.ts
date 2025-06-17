f84582a4640cb85ad99f14498d015d4720329d92
maxhu08 <contact@maxhu.dev>
2025-06-17 13:40:30 -0500

    make definition work for caps

--- src/newtab/scripts/utils/search/handle-definition.ts
+++ src/newtab/scripts/utils/search/handle-definition.ts
@@ -1,14 +1,13 @@
 export const handleDefinition = async (val: string) => {
-  const match = val.match(/\b(.+?)\s+def(?:i(?:n(?:i(?:t(?:i(?:o(?:n)?)?)?)?)?)?)?$/);
+  const match = val.match(/\b(.+?)\s+def(?:i(?:n(?:i(?:t(?:i(?:o(?:n)?)?)?)?)?)?)?$/i);
   if (!match) return;
 
-  const word = match[1];
+  const word = match[1].toLowerCase();
 
   try {
-    // prettier-ignore
-    const response = await fetch(`https://raw.githubusercontent.com/open-dictionary/english-dictionary/master/${word[0]}/${word[1]}/${word}/en.json`);
-    // const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-
+    const response = await fetch(
+      `https://raw.githubusercontent.com/open-dictionary/english-dictionary/master/${word[0]}/${word[1]}/${word}/en.json`
+    );
     const data = await response.json();
     return { type: "definition", result: data } as const;
   } catch {}
