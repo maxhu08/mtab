de39b54447ebea37f4181aa10b7c26d10d0f7db9
maxhu08 <contact@maxhu.dev>
2026-01-14 17:01:00 -0600

    fix bookmark dragging feeling unsmooth

--- src/options/scripts/utils/fill-helpers/fill-user-defined-bookmarks.ts
+++ src/options/scripts/utils/fill-helpers/fill-user-defined-bookmarks.ts
@@ -56,6 +56,7 @@ export const handleBookmarkNodesDragging = () => {
       },
       fallbackOnBody: true,
       swapThreshold: 0.65,
+      invertSwap: true,
       handle: ".bookmark-node-handle",
       animation: 250,
       easing: "cubic-bezier(0.42, 0, 0.58, 1)",
