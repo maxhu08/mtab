a2d25c9f6ce376a84b9b5c54be11e9b1a2b873ea
maxhu08 <contact@maxhu.dev>
2026-01-14 21:56:15 -0600

    add keybinds for export and import

--- src/options/scripts/keybinds.ts
+++ src/options/scripts/keybinds.ts
@@ -1,4 +1,6 @@
 import { saveConfig } from "src/options/scripts/utils/save-config";
+import { saveAndExportConfig } from "src/options/scripts/utils/export-config";
+import { importConfigAndSave } from "src/options/scripts/utils/import-config";
 
 export const listenToKeys = () => {
   document.addEventListener("keydown", (e) => {
@@ -6,6 +8,18 @@ export const listenToKeys = () => {
       e.preventDefault();
 
       saveConfig();
+    }
+
+    if (e.ctrlKey && e.key === "e") {
+      e.preventDefault();
+
+      saveAndExportConfig();
+    }
+
+    if (e.ctrlKey && e.key === "i") {
+      e.preventDefault();
+
+      importConfigAndSave();
     }
   });
 };
