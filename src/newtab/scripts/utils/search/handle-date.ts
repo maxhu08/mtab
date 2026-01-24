b47bb9e5af7f85a32db11bfff9864d8fb7a93845
maxhu08 <contact@maxhu.dev>
2026-01-23 20:45:19 -0600

    make date assist case insensitive

--- src/newtab/scripts/utils/search/handle-date.ts
+++ src/newtab/scripts/utils/search/handle-date.ts
@@ -1,7 +1,7 @@
 import { AssistDate } from "src/newtab/scripts/utils/search/search-assist-utils";
 
 export const handleDate = (val: string) => {
-  if (val.trim() === "date") {
+  if (val.trim().toLowerCase() === "date") {
     return { type: "date" } satisfies AssistDate;
   }
 };
