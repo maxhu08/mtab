ca955447ce3c43d0483d7453bbad2d7077d6c43b
maxhu08 <contact@maxhu.dev>
2026-02-05 20:24:18 -0600

    fix user-defined bookmark icons with "url-*" iconType not displaying when iconColor is unset (#88)

--- src/newtab/scripts/utils/bookmarks/bookmark-icon.ts
+++ src/newtab/scripts/utils/bookmarks/bookmark-icon.ts
@@ -7,6 +7,8 @@ export const getBookmarkIconDetails = (
   bookmarkIconType: string,
   bookmarkIconColor: string
 ): BookmarkIconResult => {
+  const iconColor = bookmarkIconColor.trim();
+
   if (bookmarkIconType.startsWith("ri-")) {
     return {
       iconHTML: `<i class="${bookmarkIconType}"></i>`,
@@ -24,7 +26,7 @@ export const getBookmarkIconDetails = (
   if (/^fa-\w+\sfa-\w+/.test(bookmarkIconType)) {
     const [first, second] = bookmarkIconType.split(/\s/).map((part) => part.slice(3));
     return {
-      iconHTML: `<span class="w-10 h-10 md:w-16 md:h-16" style="display:inline-block;background-color:${bookmarkIconColor};mask:url('/icons/fontawesome/svgs/${first}/${second}.svg') no-repeat center;-webkit-mask:url('/icons/fontawesome/svgs/${first}/${second}.svg') no-repeat center;transition:background-color 0.3s;pointer-events:none;"></span>`,
+      iconHTML: `<span class="w-10 h-10 md:w-16 md:h-16 inline-block" style="background-color:${iconColor || "transparent"};mask:url('/icons/fontawesome/svgs/${first}/${second}.svg') no-repeat center;-webkit-mask:url('/icons/fontawesome/svgs/${first}/${second}.svg') no-repeat center;transition:background-color 0.3s;pointer-events:none;"></span>`,
       iconSizeClass: ""
     };
   }
@@ -32,15 +34,29 @@ export const getBookmarkIconDetails = (
   if (bookmarkIconType.startsWith("si-")) {
     const icon = bookmarkIconType.slice(3);
     return {
-      iconHTML: `<span class="w-10 h-10 md:w-16 md:h-16" style="display:inline-block;background-color:${bookmarkIconColor};mask:url('/icons/simpleicons/${icon}.svg') no-repeat center;-webkit-mask:url('/icons/simpleicons/${icon}.svg') no-repeat center;transition:background-color 0.3s;pointer-events:none;"></span>`,
+      iconHTML: `<span class="w-10 h-10 md:w-16 md:h-16 inline-block" style="background-color:${iconColor || "transparent"};mask:url('/icons/simpleicons/${icon}.svg') no-repeat center;-webkit-mask:url('/icons/simpleicons/${icon}.svg') no-repeat center;transition:background-color 0.3s;pointer-events:none;"></span>`,
       iconSizeClass: ""
     };
   }
 
+  console.log("bookmarkIconType:", bookmarkIconType);
+
   if (bookmarkIconType.startsWith("url-")) {
     const src = bookmarkIconType.slice(4);
+    // if bookmarkIconColor is empty, use <img> tag directly without applying the mask
+
+    if (iconColor === "") {
+      return {
+        iconHTML: `<img class="w-10 md:w-14" src="${src}" alt="icon" />`,
+        iconSizeClass: ""
+      };
+    }
+
+    // if bookmarkIconColor is not empty, use mask as before
+    const maskStyles = `background-color:${iconColor};mask:url('${src}') no-repeat center/contain;-webkit-mask:url('${src}') no-repeat center/contain;transition:background-color .3s;pointer-events:none`;
+
     return {
-      iconHTML: `<span class="w-10 h-10 md:w-16 md:h-16" style="display:inline-block;background-color:${bookmarkIconColor};mask:url('${src}') no-repeat center/contain;-webkit-mask:url('${src}') no-repeat center/contain;transition:background-color .3s;pointer-events:none"></span>`,
+      iconHTML: `<span class="w-10 h-10 md:w-14 md:h-14 inline-block" style="${maskStyles}"></span>`,
       iconSizeClass: ""
     };
   }
