02c69441ad8e6de18979c0693df9c1d61e41f534
maxhu08 <contact@maxhu.dev>
2026-02-11 21:04:13 -0600

    remove unnecessary

--- src/options/scripts/init.ts
+++ src/options/scripts/init.ts
@@ -1,3 +1,54 @@
-import { loadPage } from "src/options/scripts/load-page";
+import { fillInputs } from "src/options/scripts/utils/fill-inputs";
+import { listenToKeys } from "src/options/scripts/keybinds";
+import { listenToInputs } from "src/options/scripts/inputs";
+import { handleWallpaperFileUpload } from "src/options/scripts/utils/upload-wallpaper";
+import { getConfig } from "src/utils/config";
+import { getUserAgent } from "src/utils/user-agent";
+import { handleSwitches } from "src/options/scripts/utils/handle-switch";
+import {
+  fixAllToggleCheckboxSections,
+  listenAllToggleCheckboxSections
+} from "src/options/scripts/utils/toggle-checkbox";
+import { handleCustomFaviconUpload } from "src/options/scripts/utils/upload-favicon";
+import { createCollapseGroups } from "src/options/scripts/utils/collapse-option";
+import { handleControls } from "src/options/scripts/utils/control-utils";
+import { handleWallpaperChange } from "src/options/scripts/utils/handle-wallpaper-change";
 
-loadPage();
+const logo = document.getElementById("mtab-logo") as HTMLImageElement;
+logo.classList.add("animate-up-bouncy");
+
+logo.addEventListener(
+  "animationend",
+  () => {
+    logo.classList.replace("animate-up-bouncy", "animate-float");
+  },
+  {
+    once: true
+  }
+);
+
+createCollapseGroups();
+
+getConfig(({ config }) => {
+  fillInputs(config);
+
+  fixAllToggleCheckboxSections();
+});
+
+(document.getElementById("version-number-text") as HTMLSpanElement).textContent +=
+  chrome.runtime.getManifest().version;
+
+(document.getElementById("user-agent-text") as HTMLSpanElement).textContent += getUserAgent();
+
+listenToInputs();
+listenToKeys();
+
+handleCustomFaviconUpload();
+handleWallpaperFileUpload();
+handleWallpaperChange();
+
+handleSwitches();
+
+listenAllToggleCheckboxSections();
+
+handleControls();
