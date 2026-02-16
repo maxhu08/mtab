fdf0982e4440cf7b58bee4709449f9ffb15e8df0
maxhu08 <contact@maxhu.dev>
2026-02-16 15:27:36 -0600

    filter out duplicate bookmarks in bookmark search

--- src/newtab/scripts/utils/bookmarks/flatten-bookmarks.ts
+++ src/newtab/scripts/utils/bookmarks/flatten-bookmarks.ts
@@ -1,12 +1,21 @@
 import { BookmarkNode, BookmarkNodeBookmark } from "src/utils/config";
 
 export const flattenBookmarks = (nodes: BookmarkNode[]): BookmarkNodeBookmark[] => {
-  return nodes.flatMap((node) => {
+  const flattened = nodes.flatMap((node) => {
     if (node.type === "bookmark") {
       return [node];
     } else if (node.type === "folder" && node.contents) {
       return flattenBookmarks(node.contents);
     }
     return [];
+  });
+
+  const seenBookmarks = new Set<string>();
+
+  return flattened.filter((bookmark) => {
+    const key = `${bookmark.name}\u0000${bookmark.url}`;
+    if (seenBookmarks.has(key)) return false;
+    seenBookmarks.add(key);
+    return true;
   });
 };
