ff0e35a71dc390131eebd522a6d76a3007604cf0
maxhu08 <contact@maxhu.dev>
2026-03-06 20:24:31 -0600

    refactor(options): use aria-pressed for option button selection state

--- src/options/scripts/utils/get-selected-button.ts
+++ src/options/scripts/utils/get-selected-button.ts
@@ -1,5 +1,5 @@
 export const getSelectedButton = (btnOptionType: string): HTMLButtonElement => {
   return document.querySelector(
-    `button[btn-option-type="${btnOptionType}"][selected="yes"]`
+    `button[btn-option-type="${btnOptionType}"][aria-pressed="true"]`
   ) as HTMLButtonElement;
 };
